d9d03074bd7e6537d11176cb3d9a7e90362093c5
Alexander Londoño Espejo <alexander.londono@secuencia24.com>
2016-11-07 20:52:01 -0500

    Ajuste de CORS

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -16,21 +16,12 @@ class Cors
     public function handle($request, Closure $next)
     {
 
-        header("Access-Control-Allow-Origin: *");
+            return $next($request)
+                        ->header('Access-Control-Allow-Origin', '*')
+                        ->header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT, DELETE')
+                        ->header('Access-Control-Allow-Credentials', 'true')
+                        ->header('Access-Control-Max-Age', '10000')
+                        ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With');
 
-        // ALLOW OPTIONS METHOD
-        $headers = [
-            'Access-Control-Allow-Methods'=> 'POST, GET, OPTIONS, PUT, DELETE',
-            'Access-Control-Allow-Headers'=> 'Content-Type, X-Auth-Token, Origin'
-        ];
-        if($request->getMethod() == "OPTIONS") {
-            // The client-side application can set only headers allowed in Access-Control-Allow-Headers
-            return Response::make('OK', 200, $headers);
-        }
-
-        $response = $next($request);
-        foreach($headers as $key => $value)
-            $response->header($key, $value);
-        return $response;
     }
 }
